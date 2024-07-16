4655336860055b42a873f2968f4840bc61108545
Matheus Silva <matheuskauan2709@gmail.com>
2024-07-16 18:13:49 -0300

    feat: consumes new API in the checkout component

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,41 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Restaurant } from '../pages/Home/menu'
 
+type Products = {
+    id: number,
+    price: number
+}
+
+type PurchasePayload = {
+    products: Products[]
+    delivery: {
+        receiver: string
+        address: {
+            description: string
+            city: string
+            zipCode: string
+            number: number
+            complement?: string
+        }
+        payment: {
+            card: {
+                name?: string
+                number?: string
+                code?: number
+                expires?: {
+                    month: number
+                    year: number
+                }
+            }
+        }    
+        
+
+    }
+}
+
+type PurchaseResponse = {
+    orderId: string
+  }
 
 const api = createApi({
     baseQuery: fetchBaseQuery({
@@ -12,10 +47,17 @@ const api = createApi({
         }),
         getRestaurants: builder.query<Restaurant[], void>({
             query: () => 'restaurantes'
-          })
+          }),
+        purchase: builder.mutation<PurchaseResponse, PurchasePayload>({
+            query: (body) => ({
+                url: 'checkout',
+                method: 'POST',
+                body
+            })
+        })
     })
 })
 
 export default api
 
-export const {useGetRestaurantsQuery, useGetRestaurantSelectedQuery } = api
+export const {useGetRestaurantsQuery, useGetRestaurantSelectedQuery, usePurchaseMutation } = api
